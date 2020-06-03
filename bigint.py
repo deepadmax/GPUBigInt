d6dd1c27b3c0e4a8dea193ba3c874d9c7ddecef1
deepadmax <maximillian.strand@gmail.com>
2020-06-04 01:33:54 +0200

    update carry

--- bigint.py
+++ bigint.py
@@ -2,8 +2,10 @@ import cupy as cp
 
 
 # Configurable default values
+# Don't change after start!
 DISPLAY_DIGITS = 5
 SEGMENT_LENGTH = 8
+SEGMENT_BASE = 10**SEGMENT_LENGTH
 
 # Constants
 MAX_TENSOR_VALUE = 2**63-1
@@ -14,26 +16,43 @@ def bigint(x):
         raise TypeError('x must be int')
         
     z = str(x)
+    
+    if x < 0:
+        return bigint(0) - bigint(abs(x))
+        
     z = z.zfill(len(z) + (-len(z) % SEGMENT_LENGTH))
     
     tensor = cp.array([
         int(z[i:i+SEGMENT_LENGTH])
             for i in range(0, len(z), SEGMENT_LENGTH)])
     
-    bi = BigInt(tensor)
+    return BigInt(tensor)
     
-    return bi
+def match_size(a, b):
+    diff = abs(a.size - b.size)
+    ext = cp.zeros(diff, dtype=cp.int64)
     
-    
-def partition(x):
-    z = str(x)
-    overflow = int(z[:-SEGMENT_LENGTH].zfill(1))
-    value = int(z[-SEGMENT_LENGTH:].zfill(SEGMENT_LENGTH))
-    return overflow, value
+    if a.size < b.size:
+        a = cp.concatenate((ext, a))
+    elif a.size > b.size:
+        b = cp.concatenate((ext, b))
+        
+    return a, b
 
 class BigInt:
-    def __init__(self, tensor):
-        self.tensor = tensor
+    def __init__(self, tensor, sign=0):
+        if tensor[0] < 0:
+            self.sign = -1
+            self.tensor = tensor[1:]
+                
+        else:
+            self.sign = 1
+            self.tensor = tensor
+            
+        if sign != 0:
+            self.sign = sign
+            
+        self.carry()
         
     def __repr__(self):
         z = ''.join([str(x) for x in self.tensor])
@@ -43,25 +62,60 @@ class BigInt:
         else:
             return z
             
-    def __add__(a, b):
-        if type(b) is not BigInt:
-            if type(b) is int:
-                b = BigInt(b)
-            else:
-                raise TypeError(f'must be BigInt or int, not {type(b).__name__}')
+    def __int__(self):
+        value = int(''.join([str(x) for x in self.tensor]))
+        
+        if self.sign == -1:
+            return 10**len(self.tensor) - value
+        else:
+            return value
+            
+    def __add__(a, b):        
+        a_t, b_t = match_size(a.tensor, b.tensor)
+        tensor = a_t + b_t
+        
+        if a.sign == -1 and b.sign == -1:
+            return BigInt(tensor, sign=-1)
+            
+        return BigInt(tensor)
+        
+    def __sub__(a, b):
+        a_t, b_t = match_size(a.tensor, b.tensor)
+        
+        if a.sign == -1 and b.sign == -1:
+            return BigInt(a_t + b_t, sign=-1)
+        
+    def carry(self):        
+        overflow = 0
+        
+        for i in range(len(self.tensor)-1, -1, -1):
+            self.tensor[i] += overflow
+            
+            if self.tensor[i] < 0:
+                overflow = -1
+                self.tensor[i] = SEGMENT_BASE - self.tensor[i]
                 
-        
-        a_t = a.tensor
-        b_t = b.tensor
-        diff = abs(a_t.size - b_t.size)
-        ext = cp.zeros(diff, dtype=cp.int64)
-        
-        if a_t.size < b_t.size:
-            a_t = cp.concatenate((ext, a.tensor))
-        elif b_t.size < a_t.size:
-            b_t = cp.concatenate((ext, b.tensor))
+            elif self.tensor[i] >= SEGMENT_BASE:
+                overflow = 1
+                self.tensor[i] -= SEGMENT_BASE
+                
+            else:
+                overflow = 0
         
         
-a = bigint(1234645159145845616545515615631531)
-b = bigint(154151561)
-a + b
+        self.sign = overflow
+        
+        if overflow == 1:
+            self.tensor = cp.concatenate((cp.array([1]), self.tensor))
+        elif overflow == -1:
+            self.sign = -1
+            
+            
+# a = bigint(1234)
+# print(int(a))
+# 
+# b = bigint(23456)
+# print(int(b))
+# 
+# c = a + b
+# print(c.tensor)
